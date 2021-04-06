3f5e71694d88b6c3d58c48b55b59f0bd1100b5b4
Dirk Lemmermann <dlemmermann@gmail.com>
2021-04-06 17:47:04 +0200

    Added missing module to module-info.java.

--- unitfx/src/main/java/module-info.java
+++ unitfx/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.dlsc.unitfx {
     requires transitive javafx.controls;
     requires tech.units.indriya;
+    requires tech.uom.lib.common;
 
     exports com.dlsc.unitfx;
     exports com.dlsc.unitfx.skins;
